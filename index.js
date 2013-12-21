3f8cd5b5e40268bb89db9e389fb1dbc2aad303cf
jesse keane <me@jessekeane.me>
2013-12-20 18:17:37 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,64 +1,61 @@
 var through = require('through'),
     lsstream = require('ls-stream'),
     path = require('path'),
-    ls = null;
+    ls = null
 
-module.exports = dirstream;
+module.exports = dirstream
 
 function dirstream(options) {
-
-  options = options || {};
-
   var arr = [],
-      tr = through(write, end);
+      tr = through(write, noop)
+
+  options = options || {}
+
+  return tr
 
   function write(buf) {
-    arr.push(buf.toString().trim());
-    if (!ls) processDir(arr.shift());
+    arr.push(buf.toString().trim())
+    if (!ls) processDir(arr.shift())
   }
 
   function processDir(dir) {
-
-    ls = lsstream(dir);
+    ls = lsstream(dir)
 
     ls.on('data', function (data) {
-      if (!data.stat) return;
-      if (options.ignoreExtensions && (options.ignoreExtensions.indexOf(path.extname(data.path).substring(1)) !== -1)) return;
-      if (options.onlyFiles && data.stat.isDirectory()) return;
-      if (options.noRecurse && path.dirname(data.path) != dir) return;
-      if (options.ignore && (options.ignore.indexOf(data.path) !== -1 || options.ignore.indexOf(path.dirname(data.path)) !== -1)) return;
+      if (!data.stat) return
+
+      if (options.ignoreExtensions &&
+        (options.ignoreExtensions.indexOf(
+          path.extname(data.path).slice(1)) !== -1)) return
+
+      if (options.onlyFiles && data.stat.isDirectory()) return
+
+      if (options.noRecurse) {
+        data.ignore()
+      }
+
+      if (options.ignore &&
+        (options.ignore.indexOf(data.path) !== -1 ||
+          options.ignore.indexOf(path.dirname(data.path)) !== -1)) return
       if (options.ignore) {
         var dirs = data.path.split(path.sep),
             i = 0,
-            l = dirs.length;
+            l = dirs.length
         for (; i < l; ++i) {
-          if (options.ignore.indexOf(dirs[i]) !== -1) return;
+          if (options.ignore.indexOf(dirs[i]) !== -1) return
         }
       }
-      tr.queue(data.path);
-    });
+
+      tr.queue(data.path)
+    })
 
     ls.on('end', function () {
-      if (!arr.length) {
-        ls = null;
-      } else {
-        processDir(arr.shift());
-      }
-    });
+      if (!arr.length) return tr.queue(null)
+      processDir(arr.shift())
+    })
 
-    ls.on('error', function () {
-      // no-op currently
-    });
-
+    ls.on('error', noop)
   }
-
-  function end() {
-    if (!ls) {
-      this.queue(null);
-    }
-  }
-
-  return tr;
- 
 }
 
+function noop(){}
